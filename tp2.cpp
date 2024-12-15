70a16f1d83662e33f58940f91a8b1245f8b41d4a
Asma AD <adasma964@gmail.com>
2024-12-15 11:36:42 +0100

    Add files via upload
    Signed-off-by: Asma AD <adasma964@gmail.com>

---
+++ tp2.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ char *ChargerChaine(int N)
+  {
+    char *chaine = ( char*)malloc((N+1)* sizeof(char));
+    if(chaine == NULL)
+    {
+    return NULL;
+    }
+     printf("Veuillez saisir la chaine de caracteres :\n");
+     scanf("%s", chaine);
+    return chaine;
+}
+
+int Longueur(char *ch)
+{
+    return strlen(ch);
+}
+
+void ChargerTab(char *p, char Tab[])
+{
+    strcpy(Tab, p);
+}
+void InverserTab(char Tab[], char T[], int m)
+{
+    int i;
+    for ( i = 0; i < m; i++)
+    {
+        T[i] = Tab[m - 1 - i];
+    }
+    T[m] = '\0';
+}
+
+void AfficherTab(char Tab[], int m)
+{
+    printf("%s\n", Tab);
+}
+
+int main() {
+    char *ch;
+    int n;
+    printf("Veuillez saisir la taille maximale de la chaine:\n");
+    scanf("%d", &n);
+    ch = ChargerChaine(n);
+    int m = Longueur(ch);
+    char Tab[m + 1], T[m + 1];
+    ChargerTab(ch, Tab);
+    AfficherTab(Tab, m);
+    InverserTab(Tab, T, m);
+    AfficherTab(T, m);
+    free(ch);
+    return 0;
+}
